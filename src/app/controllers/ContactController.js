8132e5af4e9ab4c8991d10284e35e826591fe14a
thiago lopes de mello <timabuntu@gmail.com>
2022-12-03 13:00:57 -0300

    feat: use class index to routes

---
+++ src/app/controllers/ContactController.js
@@ -0,0 +1,23 @@
+class ContactController {
+  index() {
+    // list registers
+  }
+
+  show() {
+    // Get one register
+  }
+
+  store() {
+    // Create new register
+  }
+
+  update() {
+    // Update one register
+  }
+
+  delete() {
+    // Delete one register
+  }
+}
+
+module.exports = new ContactController();
