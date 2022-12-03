56e649e927dcda698ea4e9036aa64e40d90a7aa4
thiago lopes de mello <timabuntu@gmail.com>
2022-12-03 12:09:19 -0300

    chore: eslint applied

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
-app.get("/", (request, response) => {
-  response.send("Hello world!");
+app.get('/', (request, response) => {
+  response.send('Hello world!');
 });
 
-app.listen(3000, () => console.log("🚀 rodando... em http://localhost:3000"));
+app.listen(3000, () => console.log('🚀 rodando... em http://localhost:3000'));
