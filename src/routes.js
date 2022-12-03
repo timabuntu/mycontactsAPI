f976ad8ebbb4d3cef1f4850ca9cd730866d57b31
thiago lopes de mello <timabuntu@gmail.com>
2022-12-03 13:15:19 -0300

    feat: created route Contact Controller

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,9 @@
 const { Router } = require('express');
 
+const ContactController = require('./app/controllers/ContactController');
+
 const router = Router();
 
-router.get('/', (request, response) => {
-  response.send('Hello world!');
-});
+router.get('/contacts', ContactController.index);
 
-
-module.exports router
+module.exports = router;
