78d2a57ce4d5d99fcb195c659be5d14d1ce72a12
thiago lopes de mello <timabuntu@gmail.com>
2022-12-04 13:05:29 -0300

    feat: mock contacts

--- src/app/repositories/ContactRepository.js
+++ src/app/repositories/ContactRepository.js
@@ -1,5 +1,14 @@
 const { uuid } = require('uuidv4');
 
+const contacts = [
+  {
+    id: uuid(),
+    name: 'Thiago',
+    email: 'thiago@mail.com',
+    phone: '111111111',
+    category_id: uuid(),
+  },
+];
 class ContactRepository {
 
 }
