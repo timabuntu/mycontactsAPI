6240ef87c4e56d4ac6791bc6231c55f0db256f57
thiago lopes de mello <timabuntu@gmail.com>
2023-01-30 09:53:34 -0300

    feat: middleware cors created

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ const routes = require('./routes');
 const app = express();
 
 app.use(express.json());
+app.use((request, response, next) => {
+  response.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  next();
+});
+
 app.use(routes);
 app.use((error, request, response, next) => {
   console.log('##### Error Handler');
